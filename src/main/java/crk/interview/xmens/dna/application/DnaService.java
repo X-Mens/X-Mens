44ffffcd02aba6119f9e29d84b5104501a2c82ac
John David Ibañez Rodriguez <john.ibanez@masiv.com>
2021-11-28 20:11:41 -0500

    Dockerizacion completa lightweight, docker compose con imagen funcionando, se eliminan una dependencia de infra en aplicacion

--- src/main/java/crk/interview/xmens/dna/application/DnaService.java
+++ src/main/java/crk/interview/xmens/dna/application/DnaService.java
@@ -3,17 +3,12 @@ package crk.interview.xmens.dna.application;
 import crk.interview.xmens.dna.domain.events.SenderDnaEvent;
 import crk.interview.xmens.dna.domain.model.Dna;
 import crk.interview.xmens.dna.domain.repository.DnaRepository;
-import crk.interview.xmens.dna.infra.events.SenderDna;
 import crk.interview.xmens.share.domain.model.DnaType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
-import java.util.concurrent.CompletableFuture;
 
 import static crk.interview.xmens.dna.infra.config.redis.CacheConfiguration.MUTANT_CACHE;
 
@@ -22,7 +17,7 @@ public class DnaService {
 
     DnaRepository adnRepository;
     SenderDnaEvent senderMessageBroker;
-    private static final Logger logger = LoggerFactory.getLogger(SenderDna.class);
+    private static final Logger logger = LoggerFactory.getLogger(DnaService.class);
 
     @Autowired
     public DnaService(DnaRepository adnRepository, SenderDnaEvent senderMessageBroker) {
